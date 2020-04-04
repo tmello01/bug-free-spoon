09a4f874c3a34a4ecbd52c832fb92fc6a67bf3fa
deliriousfish <63132415+deliriousfish@users.noreply.github.com>
2020-04-04 11:50:52 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var gameData = {
+  dust: 0,
+  dustPerClick: 1
+  dustPerClickCost: 10
+}
+
+function collectDust() {
+	gameData.dust += gameData.dustPerClick
+	document.getElementById("dustCollected").innerHTML = gameData.dust + " Dust Collected "
+}
+
+function buyDustPerClick() {
+  if (gameData.dust >= gameData.dustPerClickCost) {
+    gameData.dust -= gameData.dustPerClickCost
+    gameData.dustPerClick += 1
+    gameData.dustPerClickCost *= 2
+    document.getElementById("dustCollected").innerHTML = gameData.dust + " Dust Collected "
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Duster (Currently Tier " + gameData.dustPerClick + ") Cost: " + gameData.dustPerClickCost + " Dust"
+  }
+}
+
+var mainGameLoop = window.setInterval(function() {
+	collectDust()
+}, 1000)
